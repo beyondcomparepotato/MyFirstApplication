1d4d258602bd76177cfcad049be6af3d3927fc59
Nikolay Ponomarenko <pythonscript999@gmail.com>
2022-09-16 00:52:17 +0300

    Added button which can change a text and main text in the middle

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
         setContentView(R.layout.activity_main)
+        Log.d("MyLogMAct", "onCreated")
+    }
+
+    fun onClickTest(view: View) {
+        val text = findViewById<TextView>(R.id.textView)
+        text.text = "Android Developer :)\n" +
+                "- Kotlin\n" +
+                "- Git & Github\n" +
+                "- Firebase\n" +
+                "- OOP\n" +
+                "- Agile\n"
     }
 }
